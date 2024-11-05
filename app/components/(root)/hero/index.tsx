4a350d726cca6fcf1445b023a119a9da5a75f56f
sup25 <suparnaa51@gmail.com>
2024-11-05 16:26:40 +0545

    feat: new component to show the categories -BrowseByCategories

--- app/components/(root)/hero/index.tsx
+++ app/components/(root)/hero/index.tsx
@@ -1,11 +1,24 @@
 import { Link } from "@remix-run/react";
+import { useState } from "react";
 import { FaAngleRight } from "react-icons/fa6";
 import { LuShapes } from "react-icons/lu";
 import { MdOutlineVerified } from "react-icons/md";
+import BrowseByCategories from "~/components/browseByCategories";
+
 const Hero = () => {
+  const [showCategories, setShowCategories] = useState(false);
+  const showAllCategories = () => {
+    setShowCategories(true);
+  };
+  const hideCategories = () => {
+    setShowCategories(false);
+  };
   return (
     <div className="section">
       <div className="container">
+        {showCategories && (
+          <BrowseByCategories hideCategories={hideCategories} />
+        )}
         <div className="flex justify-between pt-20 items-center gap-10 flex-wrap">
           <div className="flex flex-col gap-4 ">
             <h2 className="text-sm w-fit text-red-400 p-1 font-Arima font-semibold border border-gray-200 bg-white">
@@ -32,12 +45,12 @@ const Hero = () => {
             </Link>
           </div>
           <div className="flex flex-col h-fit p-4 flex-wrap justify-between gap-2 rounded bg-gray-200 w-full md:max-w-[500px]">
-            <Link
-              to="/products"
-              className="inline-flex   gap-4 bg-white hover:bg-gray-100 p-5 items-center font-bold rounded transition-all duration-300 ease-in-out "
+            <div
+              onClick={showAllCategories}
+              className="inline-flex  cursor-pointer gap-4 bg-white hover:bg-gray-100 p-5 items-center font-bold rounded transition-all duration-300 ease-in-out "
             >
               <LuShapes size={25} /> Browse by categories
-            </Link>
+            </div>
             <Link
               to="/products"
               className="inline-flex gap-4  bg-white hover:bg-gray-100 p-5 items-center font-bold transition-all duration-300 ease-in-out "
