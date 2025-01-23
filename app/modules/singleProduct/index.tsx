ff626b6425837d4b85d4b171de92ed707cbb9e7c
sup25 <suparnaa51@gmail.com>
2025-01-23 23:17:15 +0545

    feat: add  quantity picker in drawer body

--- app/modules/singleProduct/index.tsx
+++ app/modules/singleProduct/index.tsx
@@ -3,11 +3,13 @@ import ProductDetails from "./components/productDetails";
 import ProductActions from "./components/productActions";
 
 import ProductCategories from "./components/productCategories";
-import QuantityPicker from "./components/quantityPicker";
+import QuantityPicker from "../../components/quantityPicker";
 import { IProduct } from "~/components/schema/Proudct.schema";
 import EsewaPayment from "~/components/esewaPayment";
+import { useState } from "react";
 
 const SingleProduct = ({ product }: { product: IProduct }) => {
+  const [localQuantity, setLocalQuantity] = useState(1);
   if (!product) {
     return (
       <div className="min-h-screen flex items-center justify-center">
@@ -29,7 +31,12 @@ const SingleProduct = ({ product }: { product: IProduct }) => {
             <ProductDetails product={product} />
 
             <ProductCategories product={product} />
-            <QuantityPicker product={product} />
+            <QuantityPicker
+              product={product}
+              quantity={localQuantity}
+              setQuantity={setLocalQuantity}
+              isSingleProduct={true}
+            />
             <ProductActions product={product} />
             <EsewaPayment amount={product.price} productName={product.title} />
           </div>
