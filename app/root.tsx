145e504a2a45350c784bc5f53e1f39eedcada754
sup25 <suparnaa51@gmail.com>
2025-01-27 13:30:12 +0545

    feat: test background color

--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="bg-[#fafafa]">
+      <body>
         <Provider store={store}>
           <ToastContainer
             position="bottom-right"
