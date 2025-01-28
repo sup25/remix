e016831726da3f0f4f47454a9a79208e07289ff1
sup25 <suparnaa51@gmail.com>
2025-01-28 21:01:22 +0545

    feat: update styles in different components

--- app/components/somethingWentWrong/index.tsx
+++ app/components/somethingWentWrong/index.tsx
@@ -1,6 +1,6 @@
 export const SomethingWentWrong = () => {
   return (
-    <div className="flex items-center justify-center text-red-500 text-2xl font-bold font-Arima">
+    <div className="flex min-h-screen items-center justify-center text-red-500 text-2xl font-bold font-Arima">
       Something went wrong please try again later
     </div>
   );
