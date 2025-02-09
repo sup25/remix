c710a661b087df23329c265cb86aa6bf689469ad
sup25 <suparnaa51@gmail.com>
2025-02-09 21:00:56 +0545

    feat: made bookmark functional

---
+++ app/.server/service/bookmarks/index.tsx
@@ -0,0 +1,33 @@
+import prisma from "~/_lib/db";
+//Get specific user Bookmarks
+export const getUserBookmarks = async (userId: number) => {
+  return prisma.bookMark.findMany({
+    where: {
+      userId: userId,
+    },
+    include: {
+      product: true,
+    },
+  });
+};
+
+//Get Bookmark
+export const getBookmark = async (userId: number, productId: number) => {
+  return prisma.bookMark.findFirst({
+    where: { userId, productId },
+  });
+};
+
+// Create Bookmark
+export async function createBookmark(userId: number, productId: number) {
+  return await prisma.bookMark.create({
+    data: { userId, productId },
+  });
+}
+
+// Delete Bookmark
+export async function deleteBookmark(userId: number, productId: number) {
+  return await prisma.bookMark.deleteMany({
+    where: { userId, productId },
+  });
+}
--- app/modules/singleProduct/components/productActions/index.tsx
+++ app/modules/singleProduct/components/productActions/index.tsx
@@ -2,6 +2,7 @@ import { CgHeart } from "react-icons/cg";
 import AddToCartButton from "~/components/addtocart";
 import { IProduct } from "~/components/schema/Proudct.schema";
 import ShareButton from "./shareButton";
+import { BookMark } from "./bookMark";
 interface ProductActionsProps {
   product: IProduct;
   quantity?: number;
@@ -19,12 +20,7 @@ const ProductActions = ({
       setQuantity={setQuantity}
     />
 
-    <button
-      className="p-3 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors"
-      aria-label="Add to wishlist"
-    >
-      <CgHeart className="h-5 w-5 text-black" />
-    </button>
+    <BookMark product={product} />
     <ShareButton />
   </div>
 );
