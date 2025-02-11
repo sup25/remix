805b16dcc2aa76d78a67835b3a51c5127ba77583
sup25 <suparnaa51@gmail.com>
2025-02-11 14:42:47 +0545

    test meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,6 +10,13 @@ export const meta: MetaFunction = () => {
   return [
     { title: "Remix | Shopping Made Easy" },
     {
+      property: "og:image",
+      content:
+        "https://res.cloudinary.com/dmufwerzv/image/upload/v1739260327/android-chrome-512x512_gje9k9.png",
+    },
+    { property: "og:image:width", content: "512" },
+    { property: "og:image:height", content: "512" },
+    {
       name: "description",
       content:
         "Discover a seamless shopping experience with Remix. Enjoy intuitive navigation, real-time product updates, and personalized recommendations.",
